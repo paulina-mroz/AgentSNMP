035d9820f5a88e6df2ca1d7dd67052df12f40fd8
PaulinaWrobel <paulina.wrobel.93@wp.pl>
2016-11-29 23:30:28 +0100

    Type class init

---
+++ Type.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <iostream>
+// #include <string>
+// #include <vector>
+// #include <list>
+
+#include "Type.h"
+#include "defines.h"
+
+Type::Type() {
+    DEBUG("Constructor");
+}
+
+Type::~Type() {
+    DEBUG("Deconstructor");
+}
---
+++ Type.h
@@ -0,0 +1,16 @@
+#ifndef TYPE_H
+#define TYPE_H
+
+#include <vector>
+#include "defines.h"
+
+class Type {
+public:
+    Type();
+    ~Type();
+};
+
+std::vector<Type> types;
+
+
+#endif /* TYPE_H */
